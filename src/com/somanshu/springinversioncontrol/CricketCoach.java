1f1427640e610b47886baa8086d384e3564aa9e4
Somanshu Singla <ssomanshus@gmail.com>
2018-03-31 15:57:16 +0530

    Adding new class

---
+++ src/com/somanshu/springinversioncontrol/CricketCoach.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.somanshu.springinversioncontrol;
+
+/**
+ * @author User
+ *
+ */
+public class CricketCoach implements Coach {
+
+	/* (non-Javadoc)
+	 * @see com.somanshu.springinversioncontrol.Coach#getInstruction()
+	 */
+	@Override
+	public String getInstruction() {
+		// TODO Auto-generated method stub
+		return "I like playing cricket , so I will decide on my own!";
+	}
+
+}
